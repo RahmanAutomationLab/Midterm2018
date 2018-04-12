5d30b1dbc1352445f8fd01c5ebb254133d48bdcd
rahmanmd <rahmanmd26@gmail.com>
2018-04-11 23:18:51 -0400

    DataReader

--- src/datastructure/DataReader.java
+++ src/datastructure/DataReader.java
@@ -1,11 +1,18 @@
 package datastructure;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Stack;
+
+import databases.ConnectDB;
+
 public class DataReader {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		/*
 		 * User API to read the below textFile and print to console.
-		 * Use BufferedReader class. 
+		 * Use BufferedReader class.
 		 * Use try....catch block to handle Exception.
 		 *
 		 * Use any databases[MongoDB, Oracle, MySql] to store data and retrieve data.
@@ -17,11 +24,53 @@ public class DataReader {
 		 * Demonstrate how to use Stack that includes push,peek,search,pop elements.
 		 * Use For Each loop/while loop/Iterator to retrieve data.
 		 */
+		ConnectDB connectDB = new ConnectDB();
+		String textFile =("C:\\Users\\rahma\\MidtemMarch2018\\src\\data\\self-driving-car");
 
-		String textFile = System.getProperty("user.dir") + "/src/data/self-driving-car.txt";
+
+				//System.getProperty("user.dir") + "/src/data/self-driving-car.txt";
+
+		FileReader fr = null;
+		BufferedReader br = null;
+		try {
+			fr = new FileReader(textFile);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+
+		}
+		try {
+			if (fr != null) {
+				br = new BufferedReader(fr);
+				String st = "";
+				while ((st = br.readLine()) != null) {
+					System.out.println(st);
+				}
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
 
 
+		finally{
+			if(fr != null){
+				try {
+					fr.close();
+				}catch(Exception ex){
+					ex.printStackTrace();
+				}
+			}
+			if(br != null){
+				try {
+					br.close();
+				}catch(Exception ex){
+					ex.printStackTrace();
+				}
+			}
+		}
 
 	}
-
 }
+
+
+
+
